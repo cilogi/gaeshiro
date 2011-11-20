dd4622a50d8a3c2e0d2e0c8d27fe96d8f6d2b919
Tim Niblett <tim.niblett@cilogi.com>
2011-11-20 20:02:00 +0000

    Session invalidation on login

--- src/main/java/com/cilogi/shiro/web/LoginServlet.java
+++ src/main/java/com/cilogi/shiro/web/LoginServlet.java
@@ -21,9 +21,11 @@
 
 package com.cilogi.shiro.web;
 
+import com.google.common.collect.Maps;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
 import org.apache.shiro.web.util.WebUtils;
 
@@ -32,6 +34,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
 
@@ -51,13 +56,40 @@ public class LoginServlet extends BaseServlet {
             UsernamePasswordToken token = new UsernamePasswordToken(username, password, rememberMe, host);
             try {
                 Subject subject = SecurityUtils.getSubject();
-                subject.login(token);
+                loginWithNewSession(token, subject);
+                //subject.login(token);
                 issueJson(response, HTTP_STATUS_OK, MESSAGE, "ok");
             } catch (AuthenticationException e) {
                 issue(MIME_TEXT_PLAIN, HTTP_STATUS_NOT_FOUND, "cannot authorize " + username + ": " + e.getMessage(), response);
             }
         } catch (Exception e) {
             issue(MIME_TEXT_PLAIN, HTTP_STATUS_INTERNAL_SERVER_ERROR, "Internal error: " + e.getMessage(), response);
+        }
+    }
+
+    /**
+     * Login and make sure you then have a new session.  This helps prevent session fixation attacks.
+     * 
+     * @param token
+     * @param subject
+     */
+    private void loginWithNewSession(UsernamePasswordToken token, Subject subject) {
+        Session originalSession = subject.getSession();
+
+        Map<Object, Object> attributes = Maps.newLinkedHashMap();
+        Collection<Object> keys = originalSession.getAttributeKeys();
+        for(Object key : keys) {
+            Object value = originalSession.getAttribute(key);
+            if (value != null) {
+                attributes.put(key, value);
+            }
+        }
+        originalSession.stop();
+        subject.login(token);
+
+        Session newSession = subject.getSession();
+        for(Object key : attributes.keySet() ) {
+            newSession.setAttribute(key, attributes.get(key));
         }
     }
 
