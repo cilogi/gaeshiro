0a22f54bb05c82334872014a457cfca7b10868a7
Tim Niblett <tim.niblett@cilogi.com>
2013-07-09 20:30:44 +0100

    fixed test bug

--- src/test/java/com/cilogi/shiro/gae/TestUserDAO.java
+++ src/test/java/com/cilogi/shiro/gae/TestUserDAO.java
@@ -50,12 +50,13 @@ public class TestUserDAO extends TestCase {
 
     public void testBase() {
         UserDAO dao = new UserDAO();
+        long startCount = dao.getCount();
         GaeUser user = new GaeUser("tim", "tim");
         user.register();
         dao.saveUser(user, true);
         GaeUser back = dao.findUser("tim");
         assertEquals(user, back);
-        assertEquals(1, dao.getCount());
+        assertEquals(1L + startCount, dao.getCount());
     }
 
 }
